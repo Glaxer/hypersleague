28ebcaa7e98de88732eed7d7accee3600ebbee47
Glaxer <Skoleernederen@live.dk>
2021-03-02 13:37:51 +0100

    Changed Login, Signup, ForgotPassword and UpdateProfile to match Header

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { Card, Button, Form, Alert } from 'react-bootstrap'
+import { Card, Button, Form, Alert, Container } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 
@@ -31,7 +31,7 @@ export default function UpdateProfile() {
         }
 
         Promise.all(promises).then(() => {
-            history.push('/')
+            history.push('/profile')
         }).catch(() => {
             setError('Failed to update account')
         }).finally(() => {
@@ -40,34 +40,39 @@ export default function UpdateProfile() {
     }
 
     return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Update Profile</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required
-                                defaultValue={currentUser.email} />
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef}
-                                placeholder="Leave blank to keep the same" />
-                        </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef}
-                                placeholder="Leave blank to keep the same" />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">Update</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2">
-                <Link to="/">Cancel</Link>
+        <Container
+            className="d-flex align-items-center justify"
+            style={{ minHeight: "100vh" }}
+        >
+            <div className="w-100" style={{ maxWidth: "400px" }}>
+                <Card>
+                    <Card.Body>
+                        <h2 className="text-center mb-4">Update Profile</h2>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group id="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" ref={emailRef} required
+                                    defaultValue={currentUser.email} />
+                            </Form.Group>
+                            <Form.Group id="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" ref={passwordRef}
+                                    placeholder="Leave blank to keep the same" />
+                            </Form.Group>
+                            <Form.Group id="password-confirm">
+                                <Form.Label>Password Confirmation</Form.Label>
+                                <Form.Control type="password" ref={passwordConfirmRef}
+                                    placeholder="Leave blank to keep the same" />
+                            </Form.Group>
+                            <Button disabled={loading} className="w-100" type="submit">Update</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+                <div className="w-100 text-center mt-2">
+                    <Link to="/profile">Cancel</Link>
+                </div>
             </div>
-        </>
+        </Container>
     )
 }
