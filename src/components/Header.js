839097f0f8a9819b89d4fa9a6e673bebac35ac9b
Glaxer <JacobKjaer@outlook.dk>
2021-03-06 15:09:14 +0100

    Added Logo to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Logo from "../images/logo.png";
 
 export default function Header() {
-    return (
-        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-            <Navbar.Brand href="/">Home</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/matches">Kampe</Nav.Link>
-                    <Nav.Link href="/standings">Stilling</Nav.Link>
-                </Nav>
-                <Nav>
-                    {/* <Nav.Link href="profile">Profile</Nav.Link>
+  return (
+    <Navbar sticky="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Navbar.Brand href="/">
+        <img
+          src={Logo}
+          width="50"
+          height="50"
+          className="d-inline-block align-top"
+          alt="Hypers League logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/matches">Kampe</Nav.Link>
+          <Nav.Link href="/standings">Stilling</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/rules">Regler</Nav.Link>
+          <Nav.Link href="/about">Om os</Nav.Link>
+          <Nav.Link href="/sponsors">Sponsorer</Nav.Link>
+          {/* <Nav.Link href="profile">Profile</Nav.Link>
                     <Nav.Link href="login">Login</Nav.Link>
                     <Nav.Link href="signup">Sign Up</Nav.Link> */}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
