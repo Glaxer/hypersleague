61054c1adf14bb183e91629273524c21ef60ee80
Glaxer <JacobKjaer@outlook.dk>
2021-03-06 15:08:59 +0100

    Added first draft to Standings and UpcomingMatches

--- src/pages/Standings.js
+++ src/pages/Standings.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Container, Table } from "react-bootstrap";
 
 export default function Standings() {
-    return (
-        <div>
-            <h1>WIP</h1>
-        </div>
-    )
+  return (
+    <Container style={{ minHeight: "100vh" }}>
+      <Table responsive table-layout="fixed" size="">
+        <thead>
+          <tr>
+            <th colSpan="1">Hold</th>
+            <th>K</th>
+            <th>V</th>
+            <th>T</th>
+            <th>U</th>
+            <th>D</th>
+            <th>P</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Hold 1</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Hold 2</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Hold 3</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Hold 4</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Hold 5</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Hold 6</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Hold 7</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Hold 8</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
--- src/pages/UpcomingMatches.js
+++ src/pages/UpcomingMatches.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Table, Container } from "react-bootstrap";
 
 export default function UpcomingMatches() {
-    return (
-        <div>
-            <h1>WIP</h1>
-        </div>
-    )
+  return (
+    <Container style={{ minHeight: "100vh" }}>
+      <Table responsive table-layout="fixed" size="">
+        <thead>
+          <tr>
+            <th colSpan="5">Uge</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Tidspunkt</td>
+            <td>Team 1</td>
+            <td>vs.</td>
+            <td>Team 2</td>
+            <td>Format</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Tidspunkt</td>
+            <td>Team 1</td>
+            <td>vs.</td>
+            <td>Team 2</td>
+            <td>Format</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Tidspunkt</td>
+            <td>Team 1</td>
+            <td>vs.</td>
+            <td>Team 2</td>
+            <td>Format</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Tidspunkt</td>
+            <td>Team 1</td>
+            <td>vs.</td>
+            <td>Team 2</td>
+            <td>Format</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Tidspunkt</td>
+            <td>Team 1</td>
+            <td>vs.</td>
+            <td>Team 2</td>
+            <td>Format</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>Tidspunkt</td>
+            <td>Team 1</td>
+            <td>vs.</td>
+            <td>Team 2</td>
+            <td>Format</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
