dc2d958dff86d638841192a28397c28fe54b930b
Glaxer <JacobKjaer@outlook.dk>
2021-03-06 15:09:33 +0100

    First draft of the Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React from "react";
+import { Button, Container, Row, Col, Table } from "react-bootstrap";
+import "../css/Dashboard.css";
 
 export default function Dashboard() {
-    
-
-    return (
-        <div>
-            Hello World
+  return (
+    <>
+      <div class="bg-image">
+        <div class="container h-100">
+          <div class="row h-100 align-items-center">
+            <div class="col-12 text-right">
+              <h1 className="headerText">This is Hypers League</h1>
+              <Button variant="dark" size="lg">
+                Tilmeld jer nu
+              </Button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Container>
+              <Table responsive table-layout="fixed">
+                <thead>
+                  <tr>
+                    <th colSpan="5">Kommende Kampe</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Tidspunkt</td>
+                    <td>Team 1</td>
+                    <td>vs.</td>
+                    <td>Team 2</td>
+                    <td>Format</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Tidspunkt</td>
+                    <td>Team 1</td>
+                    <td>vs.</td>
+                    <td>Team 2</td>
+                    <td>Format</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Tidspunkt</td>
+                    <td>Team 1</td>
+                    <td>vs.</td>
+                    <td>Team 2</td>
+                    <td>Format</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Tidspunkt</td>
+                    <td>Team 1</td>
+                    <td>vs.</td>
+                    <td>Team 2</td>
+                    <td>Format</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Tidspunkt</td>
+                    <td>Team 1</td>
+                    <td>vs.</td>
+                    <td>Team 2</td>
+                    <td>Format</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Container>
+          </Col>
+          <Col>
+            <div className="dashboard-standings">
+              <h4>Tjek den nuværende stilling for ligaen</h4>
+              <Button href="/standings" variant="dark">
+                Stilling
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
