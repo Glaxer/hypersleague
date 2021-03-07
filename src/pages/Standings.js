c618ebc5dcf28a8aa2794d8cc06c2563e0ec9360
Glaxer <JacobKjaer@outlook.dk>
2021-03-07 16:22:59 +0100

    Added a wrapper with styling

--- src/pages/Standings.js
+++ src/pages/Standings.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, Table } from "react-bootstrap";
-import '../css/Standings.css'
+import "../css/Standings.css";
 
 export default function Standings() {
   const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
@@ -16,212 +16,214 @@ export default function Standings() {
 
   return (
     <Container style={{ minHeight: "100vh" }}>
-      <h1 className="header">Stilling</h1>
-      {isDesktop ? (
-        <Table responsive table-layout="fixed">
-          <thead>
-            <tr>
-              <th colSpan="1">Hold</th>
-              <th>K</th>
-              <th>V</th>
-              <th>T</th>
-              <th>U</th>
-              <th>D</th>
-              <th>P</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Hold 1</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 2</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 3</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 4</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 5</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 6</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 7</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 8</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-        </Table>
-      ) : (
-        <Table responsive table-layout="fixed" size="sm">
-          <thead>
-            <tr>
-              <th colSpan="1">Hold</th>
-              <th>K</th>
-              <th>V</th>
-              <th>T</th>
-              <th>U</th>
-              <th>D</th>
-              <th>P</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Hold 1</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 2</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 3</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 4</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 5</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 6</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 7</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Hold 8</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-        </Table>
-      )}
+      <div className="wrapper">
+        <h1 className="header">Stilling</h1>
+        {isDesktop ? (
+          <Table responsive table-layout="fixed">
+            <thead>
+              <tr>
+                <th colSpan="1">Hold</th>
+                <th>K</th>
+                <th>V</th>
+                <th>T</th>
+                <th>U</th>
+                <th>D</th>
+                <th>P</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Hold 1</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 2</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 3</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 4</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 5</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 6</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 7</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 8</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </Table>
+        ) : (
+          <Table responsive table-layout="fixed" size="sm">
+            <thead>
+              <tr>
+                <th colSpan="1">Hold</th>
+                <th>K</th>
+                <th>V</th>
+                <th>T</th>
+                <th>U</th>
+                <th>D</th>
+                <th>P</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Hold 1</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 2</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 3</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 4</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 5</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 6</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 7</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Hold 8</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
+      </div>
     </Container>
   );
 }
--- src/pages/UpcomingMatches.js
+++ src/pages/UpcomingMatches.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Table, Container } from "react-bootstrap";
-import '../css/UpcomingMatches.css'
+import "../css/UpcomingMatches.css";
 
 export default function UpcomingMatches() {
   const [isDesktop, setDesktop] = useState(window.innerWidth > 900);
@@ -16,132 +16,134 @@ export default function UpcomingMatches() {
 
   return (
     <Container style={{ minHeight: "100vh" }}>
-      <h1 className="header">Kampe</h1>
-      {isDesktop ? (
-        <Table responsive table-layout="fixed" size="">
-          <thead>
-            <tr>
-              <th colSpan="5">Uge</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-        </Table>
-      ) : (
-        <Table responsive table-layout="fixed" size="sm">
-          <thead>
-            <tr>
-              <th colSpan="5">Uge</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>Tidspunkt</td>
-              <td>Team 1</td>
-              <td>vs.</td>
-              <td>Team 2</td>
-              <td>Format</td>
-            </tr>
-          </tbody>
-        </Table>
-      )}
+      <div className="wrapper">
+        <h1 className="header">Kampe</h1>
+        {isDesktop ? (
+          <Table responsive table-layout="fixed" size="">
+            <thead>
+              <tr>
+                <th colSpan="5">Uge</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+          </Table>
+        ) : (
+          <Table responsive table-layout="fixed" size="sm">
+            <thead>
+              <tr>
+                <th colSpan="5">Uge</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tidspunkt</td>
+                <td>Team 1</td>
+                <td>vs.</td>
+                <td>Team 2</td>
+                <td>Format</td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
+      </div>
     </Container>
   );
 }
